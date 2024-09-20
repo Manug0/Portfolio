e891f093ff1890ab70d3e937dec1cd4137355fc0
Manu <manillar12@gmail.com>
2024-09-20 15:16:33 +0200

    complete responsive Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { FaRegUser } from "react-icons/fa";
 import { FaProjectDiagram } from "react-icons/fa";
 import { FaFileAlt } from "react-icons/fa";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { AiOutlineClose } from "react-icons/ai"; // Icono de cierre
 
 const HeaderContainer = styled.nav`
 	position: fixed;
@@ -25,6 +26,10 @@ const NavContent = styled.div`
 	display: flex;
 	justify-content: space-evenly;
 	align-items: center;
+
+	@media (max-width: 768px) {
+		justify-content: normal;
+	}
 `;
 
 const Logo = styled.img`
@@ -32,13 +37,36 @@ const Logo = styled.img`
 	left: 50px;
 	width: 70px;
 	height: 70px;
-	border-radius: 20px;
+
+	@media (max-width: 1000px) {
+		left: 0px;
+		width: 50px;
+		height: 50px;
+	}
+
+	@media (max-width: 768px) {
+		position: absolute;
+		left: 30px;
+	}
 `;
 
 const NavList = styled.ul`
 	list-style: none;
 	display: flex;
 	gap: 40px;
+
+	@media (max-width: 768px) {
+		flex-direction: column;
+		position: fixed;
+		top: 0;
+		right: ${(props) => (props.open ? "0" : "-100%")};
+		width: 70%;
+		height: 100vh;
+		background-color: var(--background-color);
+		transition: right 0.3s ease-in-out;
+		padding: 6rem 20px 20px;
+		box-shadow: -2px 0 5px rgba(0, 0, 0, 0.1);
+	}
 `;
 
 const NavItem = styled(NavLink)`
@@ -71,6 +99,16 @@ const NavItem = styled(NavLink)`
 	&:hover::after {
 		width: 100%;
 	}
+
+	@media (max-width: 1000px) {
+		font-size: 1rem;
+	}
+
+	@media (max-width: 768px) {
+		&:hover::after {
+			width: 0%;
+		}
+	}
 `;
 
 const NavlinkContainer = styled.div`
@@ -84,10 +122,35 @@ const HeaderButtons = styled.div`
 
 const NavbarToggle = styled.label`
 	display: none;
+	font-size: 1.3rem;
+
+	@media (max-width: 768px) {
+		display: block;
+		cursor: pointer;
+		position: absolute;
+		right: 30px;
+		top: 40px;
+	}
+`;
+
+// Fondo oscurecido/borroso al abrir el menú
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0.5);
+	backdrop-filter: blur(5px);
+	z-index: 98;
+	opacity: ${(props) => (props.open ? "1" : "0")};
+	visibility: ${(props) => (props.open ? "visible" : "hidden")};
+	transition: opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
 `;
 
 const Header = () => {
 	const [blur, setBlur] = useState(false);
+	const [navOpen, setNavOpen] = useState(false);
 
 	const toggleHeaderBlur = () => {
 		if (window.scrollY > 0) {
@@ -95,6 +158,16 @@ const Header = () => {
 		} else {
 			setBlur(false);
 		}
+	};
+
+	// Cerrar el menú si se hace clic fuera de él
+	const handleOverlayClick = () => {
+		setNavOpen(false);
+	};
+
+	// Cerrar el menú si se hace clic en un enlace
+	const handleNavItemClick = () => {
+		setNavOpen(false);
 	};
 
 	useEffect(() => {
@@ -105,43 +178,46 @@ const Header = () => {
 	}, []);
 
 	return (
-		<HeaderContainer blur={blur}>
-			<NavContent>
-				<Logo src="/Logo.svg" alt="logo" />
-				<NavList>
-					<NavItem to="/">
-						<NavlinkContainer>
-							<GoHome />
-							&nbsp; Inicio
-						</NavlinkContainer>
-					</NavItem>
+		<>
+			<HeaderContainer blur={blur}>
+				<NavContent>
+					<Logo src="/Logo.svg" alt="logo" />
+					<NavList open={navOpen}>
+						<NavItem to="/" onClick={handleNavItemClick}>
+							<NavlinkContainer>
+								<GoHome />
+								&nbsp; Inicio
+							</NavlinkContainer>
+						</NavItem>
 
-					<NavItem to="/about">
-						<NavlinkContainer>
-							<FaRegUser size={"0.8em"} />
-							&nbsp; Sobre mí
-						</NavlinkContainer>
-					</NavItem>
-					<NavItem to="/projects">
-						<NavlinkContainer>
-							<FaProjectDiagram size={"0.8em"} />
-							&nbsp; Proyectos
-						</NavlinkContainer>
-					</NavItem>
-					<NavItem to="/CV">
-						<NavlinkContainer>
-							<FaFileAlt size={"0.8em"} />
-							&nbsp;Currículum
-						</NavlinkContainer>
-					</NavItem>
-				</NavList>
-				<HeaderButtons>
-					<NavbarToggle>
-						<RxHamburgerMenu />
-					</NavbarToggle>
-				</HeaderButtons>
-			</NavContent>
-		</HeaderContainer>
+						<NavItem to="/about" onClick={handleNavItemClick}>
+							<NavlinkContainer>
+								<FaRegUser size={"0.8em"} />
+								&nbsp; Sobre mí
+							</NavlinkContainer>
+						</NavItem>
+						<NavItem to="/projects" onClick={handleNavItemClick}>
+							<NavlinkContainer>
+								<FaProjectDiagram size={"0.8em"} />
+								&nbsp; Proyectos
+							</NavlinkContainer>
+						</NavItem>
+						<NavItem to="/CV" onClick={handleNavItemClick}>
+							<NavlinkContainer>
+								<FaFileAlt size={"0.8em"} />
+								&nbsp; Currículum
+							</NavlinkContainer>
+						</NavItem>
+					</NavList>
+					<HeaderButtons>
+						<NavbarToggle onClick={() => setNavOpen(!navOpen)}>
+							{navOpen ? <AiOutlineClose /> : <RxHamburgerMenu />}
+						</NavbarToggle>
+					</HeaderButtons>
+				</NavContent>
+			</HeaderContainer>
+			<Overlay open={navOpen} onClick={handleOverlayClick} />
+		</>
 	);
 };
 
